c7774ad8fe9f83e616d324e0122bd9745819a9eb
rubavila94 <rubavila94@gmail.com>
2024-09-20 18:30:31 +0200

    Revert "Primer commit"
    This reverts commit f83ba8ffacc4cbcff6cccea74ed2e1e916733663.

--- proyecto03-springboot-prueba-002/src/main/java/com/tcna/springdatajpa/repositories/repository2/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tcna.springdatajpa.repositories.repository2;
-
-import com.tcna.springdatajpa.entities.entity2.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AutorRepository extends JpaRepository<Autor,Long> {
-}
